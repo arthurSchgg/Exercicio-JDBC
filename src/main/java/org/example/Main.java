6b71cfe81cd57c13c591915ef6ccda5aa22f364e
Arthur Miguel Schlichting <arthurms2904@gmail.com>
2026-07-22 20:30:57 -0300

    Ajutes finais do código

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import org.example.dao.ContatoDao;
 import org.example.model.Contato;
@@ -14,10 +15,7 @@ public class Main {
     }
 
     public static void inicio() {
-        int opcao;
-
-        do {
-            
+        boolean sair = false;
         System.out.println("""
                 -----Lista de Contatos-----
                 1 - Cadastrar contato
@@ -27,44 +25,45 @@ public class Main {
                 5 - Listar por vários IDs
                 0 - Sair
                 Escolha uma opção:""");
-        opcao = SC.nextInt();
+        int opcao = SC.nextInt();
         SC.nextLine();
 
-            switch (opcao) {
-                case 1: {
-                    cadastrarContato();
-                    break;
-                }
-    
-                case 2: {
-                    editarContato();
-                    break;
-                }
-    
-                case 3: {
-                    listarContatos();
-                    break;
-                }
-    
-                case 4: {
-                    buscarContatoPorNome();
-                    break;
-                }
-    
-                case 5: {
-                    listarPorVariosIDs();
-                    break;
-                }
-
-                case 0: {
-                    System.out.println("Saindo...");
-                    break;
-                }
-    
-                default:
-                    throw new AssertionError();
+        switch (opcao) {
+            case 1: {
+                cadastrarContato();
+                break;
             }
-        } while(opcao != 0);
+
+            case 2: {
+                editarContato();
+                break;
+            }
+
+            case 3: {
+                listarContatos();
+                break;
+            }
+
+            case 4: {
+                buscarContatoPorNome();
+                break;
+            }
+
+            case 5: {
+                listarPorVariosIDs();
+                break;
+            }
+
+            case 0: {
+                sair = true;
+                break;
+            }
+            default:
+                throw new AssertionError();
+        }
+        if (!sair) {
+            inicio();
+        }
     }
 
     public static void cadastrarContato() {
@@ -88,16 +87,37 @@ public class Main {
     public static void editarContato() {
 
         var contatoDao = new ContatoDao();
+        List<Contato> contatos = new ArrayList<>();
+        List<Integer> idsContatos = new ArrayList<>();
 
         try {
-            System.out.println(contatoDao.listarContatos());
+            contatos = contatoDao.listarContatos();
         } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        for (Contato contato : contatos) {
+            System.out.println("==========================");
+            System.out.println("ID: " + contato.getId());
+            System.out.println("Nome: " + contato.getNome());
+            System.out.println("Número: " + contato.getNumero());
+            System.out.println("==========================");
+        }
+
+        try {
+            idsContatos = contatoDao.buscarIDs();
+        } catch (SQLException e){
             e.printStackTrace();
         }
 
         System.out.print("Digite o id do contato que você deseja editar: ");
         int id = SC.nextInt();
         SC.nextLine();
+
+        if(!idsContatos.contains(id)){
+            System.out.println("Id não existe!");
+            return;
+        }
 
         System.out.print("Digite o novo nome do contato: ");
         String novoNome = SC.nextLine();
@@ -112,15 +132,25 @@ public class Main {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        System.out.println("Contato atualizado com sucesso!");
     }
 
     public static void listarContatos() {
         var contatoDao = new ContatoDao();
+        List<Contato> contatos = new ArrayList<>();
 
         try {
-            System.out.println(contatoDao.listarContatos());
+            contatos = contatoDao.listarContatos();
         } catch (SQLException e) {
             e.printStackTrace();
+        }
+
+        for (Contato contato : contatos) {
+            System.out.println("==========================");
+            System.out.println("ID: " + contato.getId());
+            System.out.println("Nome: " + contato.getNome());
+            System.out.println("Número: " + contato.getNumero());
+            System.out.println("==========================");
         }
     }
 
@@ -129,38 +159,47 @@ public class Main {
         String nome = SC.nextLine();
 
         var contatoDao = new ContatoDao();
+        List<Contato> listaContatos = new ArrayList<>();
 
-        try{
+        try {
             System.out.println(contatoDao.buscarContatoPorNome(nome));
-        } catch (SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
+        }
+
+        for (Contato contato : listaContatos) {
+            System.out.println("==========================");
+            System.out.println("ID: " + contato.getId());
+            System.out.println("Nome: " + contato.getNome());
+            System.out.println("Número: " + contato.getNumero());
+            System.out.println("==========================");
         }
     }
 
-    public static void listarPorVariosIDs(){
+    public static void listarPorVariosIDs() {
         var listaIDs = new ArrayList<Integer>();
         int op;
 
-        do{
+        do {
             System.out.println("Digite o ID que deseja buscar: ");
             int id = SC.nextInt();
 
             listaIDs.add(id);
 
             System.out.println("""
-                Deseja continuar buscando IDs? 
-                [1] Sim     
-                [2] Não
-                Escolha uma opção: """);
-                op = SC.nextInt();
-                
-        } while(op == 1);
+                    Deseja continuar buscando IDs?
+                    [1] Sim
+                    [2] Não
+                    Escolha uma opção: """);
+            op = SC.nextInt();
+
+        } while (op == 1);
 
         var contatoDao = new ContatoDao();
 
-        try{
+        try {
             System.out.println(contatoDao.listarPorVariosIDs(listaIDs));
-        } catch(SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
--- src/main/java/org/example/dao/ContatoDao.java
+++ src/main/java/org/example/dao/ContatoDao.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.example.model.Contato;
@@ -48,9 +49,12 @@ public class ContatoDao {
         }
     }
 
-    public StringBuilder listarContatos() throws SQLException {
+    public List<Contato> listarContatos() throws SQLException {
+        List<Contato> listaContatos = new ArrayList<>();
         String command = """
-                SELECT id, nome, numero
+                SELECT id
+                , nome
+                , numero
                 FROM contatos
                 """;
         try (Connection conn = ConnectionFactory.conectar();
@@ -58,25 +62,24 @@ public class ContatoDao {
 
             ResultSet rs = stmt.executeQuery();
 
-            var listaContatos = new StringBuilder();
-
             while (rs.next()) {
                 int id = rs.getInt("id");
                 String nome = rs.getString("nome");
                 String numero = rs.getString("numero");
 
                 Contato contato = new Contato(id, nome, numero);
-                listaContatos.append(contato).append("\n");
+                listaContatos.add(contato);
             }
             return listaContatos;
         }
     }
 
-    public StringBuilder buscarContatoPorNome(String nome) throws SQLException {
+    public List<Contato> buscarContatoPorNome(String nome) throws SQLException {
+        List<Contato> listaContatos = new ArrayList<>();
         String command = """
-                SELECT id,
-                nome,
-                numero
+                SELECT id
+                , nome
+                , numero
                 FROM contatos
                 WHERE
                 nome LIKE ?
@@ -85,23 +88,24 @@ public class ContatoDao {
         try (Connection conn = ConnectionFactory.conectar();
                 PreparedStatement stmt = conn.prepareStatement(command)) {
 
-            var listaContatos = new StringBuilder();
-
             stmt.setString(1, "%" + nome + "%");
 
             ResultSet rs = stmt.executeQuery();
 
-            Contato contato = null;
-
             while (rs.next()) {
-                contato = new Contato(rs.getInt("id"), rs.getString("nome"), rs.getString("numero"));
-                listaContatos.append(contato).append("\n");
+                int id = rs.getInt("id");
+                String nomeContato = rs.getString("nome");
+                String numero = rs.getString("numero");
+
+                Contato contato = new Contato(id, nomeContato, numero);
+                listaContatos.add(contato);
             }
             return listaContatos;
         }
     }
 
-    public StringBuilder listarPorVariosIDs(List<Integer> listaIDs) throws SQLException {
+    public List<Contato> listarPorVariosIDs(List<Integer> listaIDs) throws SQLException {
+        List<Contato> listaContatos = new ArrayList<>();
         StringBuilder placeholders = new StringBuilder();
         for (int i = 0; i < listaIDs.size(); i++) {
             placeholders.append("?");
@@ -110,14 +114,15 @@ public class ContatoDao {
             }
         }
         String command = """
-                    SELECT id,
-                    nome,
-                    numero
+                    SELECT id
+                        , nome
+                        , numero
                     FROM contatos
                     WHERE
                     id IN (%s)
                 """.formatted(placeholders); /*
-                                              * .formatted(placeholders) é para os ? que no caso são os IDs que o usuário
+                                              * .formatted(placeholders) é para os ? que no caso são os IDs que o
+                                              * usuário
                                               * irá inserir, onde o (placeholders) é os "inputs" do usuário.
                                               */
 
@@ -130,7 +135,6 @@ public class ContatoDao {
             }
 
             ResultSet rs = stmt.executeQuery();
-            var listaContatos = new StringBuilder();
 
             while (rs.next()) {
                 int id = rs.getInt("id");
@@ -138,9 +142,30 @@ public class ContatoDao {
                 String numero = rs.getString("numero");
 
                 Contato contato = new Contato(id, nome, numero);
-                listaContatos.append(contato).append("\n");
+                listaContatos.add(contato);
             }
             return listaContatos;
+        }
+    }
+
+    public List<Integer> buscarIDs() throws SQLException {
+        List<Integer> ids = new ArrayList<>();
+        String command = """
+                SELECT id
+                FROM contatos
+                """;
+
+        try (Connection conn = ConnectionFactory.conectar();
+                PreparedStatement stmt = conn.prepareStatement(command)) {
+
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+
+                ids.add(id);
+            }
+            return ids;
         }
     }
 }
